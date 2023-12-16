8f3b7d24f7c0e3066e5c8fcc7c593e6d8d6b6406
Tomohiro Yoshida <rollwithit0117@gmail.com>
2023-12-16 23:47:42 +0900

    initial commit

---
+++ codezine-scraper/scraper/scraper/items.py
@@ -0,0 +1,14 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+from dataclasses import dataclass
+
+@dataclass
+class ScraperItem():
+    title: str
+    link: str
+    date: str
+    content: str
+    tag_list: list[str]
