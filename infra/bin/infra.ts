666ef3d078482cd7366f8a459577c9ffd187177d
Tomohiro Yoshida <rollwithit0117@gmail.com>
2024-01-10 22:23:24 +0900

    feat: add sfn

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { LambdaStack } from '../lib/lambda/lambda-stack';
+// import { LambdaStack } from '../lib/lambda/lambda-stack';
+import { StepFunctionsStack } from '../lib/step-functions/step-functions-stack';
 import { S3Stack } from '../lib/s3/s3-stack';
 
 const app = new cdk.App();
@@ -10,6 +11,9 @@ const env: cdk.Environment = {
   region: 'ap-northeast-1',
 }
 
-new LambdaStack(app, 'PNST-LAMBDA', { env, });
+// new LambdaStack(app, 'PNST-LAMBDA', { env, });
 
-new S3Stack(app, 'PNST-S3', { env, });
+const s3Stack = new S3Stack(app, 'PNST-S3', { env, });
+
+new StepFunctionsStack(app, 'PNST-STEP-FUNCTIONS', { env, })
+  .addDependency(s3Stack);
